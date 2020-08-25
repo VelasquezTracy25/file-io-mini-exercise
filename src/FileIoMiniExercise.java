538eedf1bcede95833b4a7b7eb2067fbe168771f
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-08-25 15:16:51 -0500

    completed exercise

--- src/FileIoMiniExercise.java
+++ src/FileIoMiniExercise.java
@@ -6,42 +6,69 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileIoMiniExercise {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // TODO: define the path to the instructor-names.txt file
-
-
+        //both of these work:
+        //Path path = Paths.get("src", "instructor-names.txt");
+        Path path = Paths.get("./src/instructor-names.txt");
         // TODO: uncomment the following variable declarations
-//        List<Instructor> instructors;
-//        List<String> names = new ArrayList<>();
+        List<Instructor> instructors;
+        List<String> names = new ArrayList<>();
 
         // TODO: read the contents of the instructor-names.txt file and store the list of strings into the 'names' variable
 
+        try {
+            names = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //intellij recommended adding throws IOException to psvm
+        System.out.println(names);
 
         // TODO: assign the 'instructors' variable a list of Instructor objects with names matching the list of strings/names from the text file (the Instructor class has a helpful method for this)
+
+        instructors = Instructor.nameStringsToInstructors(names);
+        //used namesStringsToInstructors() method in Instructor class
 
 
         // TODO: greet all instructors by their names
 
+        greetInstructors(instructors);
+        //uses existing method in FileIoMiniExercise
 
         // TODO: change "Fred" to "David" in the list of Instructor objects
 
+        changeFredToDavid(instructors);
+        System.out.println(names);
+        //uses existing method in FileIoMiniExercise *****
 
         // TODO: update the list of strings/names ('names' variable) to the latest names from the Instructor objects (the Instructor class has a helpful method for this)
 
+        names = Instructor.instructorsToNameStrings(instructors);
 
         // TODO: overwrite the instructors-names.txt file with the latest list of names
+        try {
+            Files.write(path, names);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         // TODO: uncomment the following line
-//        System.out.println("============== after name change...");
+        System.out.println("============== after name change...");
 
         // TODO: greet instructors again (no need to reread the text file again; just use the existing list of instructors in memory)
 
+        greetInstructors(instructors);
 
     }
 
     /**
      * This method takes in a list of Instructor objects and prints out "Hello, NAME!" for each Instructor object's name
+     *
      * @param instructors is a list of Instructor objects
      */
     public static void greetInstructors(List<Instructor> instructors) {
@@ -52,6 +79,7 @@ public class FileIoMiniExercise {
 
     /**
      * This method takes in a list of Instructor objects and changes the name of the Instructor with a name of "Fred" to "David"
+     *
      * @param instructors is a list of Instructor objects
      */
     public static void changeFredToDavid(List<Instructor> instructors) {
